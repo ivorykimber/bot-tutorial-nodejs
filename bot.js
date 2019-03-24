846e5007c19e62586e32fcde4dbaba5a5ce4e96c
ivorykimber <47680628+ivorykimber@users.noreply.github.com>
2019-03-24 10:01:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,7 @@ function respond() {
   }
 }
 
+
 function sheetLink() {
   var botResponse, options, body, botReq;
 
@@ -40,6 +41,25 @@ function sheetLink() {
     "text" : botResponse
   };
 
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+    
 function nameResponse() {
   var botResponse, options, body, botReq;
 
