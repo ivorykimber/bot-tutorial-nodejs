12ddab068b3070afc60774fd7aca2a1d67fb2356
ivorykimber <47680628+ivorykimber@users.noreply.github.com>
2019-03-31 13:20:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,10 @@ function respond() {
     this.res.writeHead(200);
     sender(request.text, request.name);
     this.res.end();
+  } else if(request.text && (request.text.indexOf("!social") == 0)) {
+    this.res.writeHead(200);
+    socialLink();
+    this.res.end();
   } else if(request.text && nameRegex.test(request.text)) {
     this.res.writeHead(200);
     nameResponse();
@@ -169,6 +173,42 @@ function berkeleyShade() {
   });
   botReq.end(JSON.stringify(body));
 }
+
+function socialLink() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "Here is the link: https://bit.ly/2FMwDxm";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+    
 
 function rando(tempTxt) {
   var botResponse, options, body, botReq;
